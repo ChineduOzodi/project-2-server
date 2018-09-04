b77121620387a0a153fd1d6945d9af963542aeaa
Eddie Grays <eddiegrays10@gmail.com>
2018-09-04 02:03:14 -0400

    current, need to clearify before continueing

---
+++ src/main/java/com/revature/services/ConfirmUser.java
@@ -0,0 +1,15 @@
+package com.revature.services;
+
+import com.revature.beans.User;
+
+public class ConfirmUser {
+
+	public User confirm(User user) {
+
+		User actualUser = null;
+
+		return actualUser;
+
+	}
+
+}
