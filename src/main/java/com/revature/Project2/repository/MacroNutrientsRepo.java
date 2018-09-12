087c29800abe9608f5b7c15f76af333a31e5606e
Chinedu Ozodi <chinedu.ozodi@gmail.com>
2018-09-12 13:57:26 -0400

    moved up the project from it's folder

---
+++ src/main/java/com/revature/Project2/repository/MacroNutrientsRepo.java
@@ -0,0 +1,13 @@
+package com.revature.Project2.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.revature.Project2.beans.MacroNutrients;
+
+public interface MacroNutrientsRepo extends CrudRepository<MacroNutrients, Integer>{
+	
+	/*
+	 * CrudRepository for MacroNutrients Bean
+	 */
+
+}
---
+++ src/main/java/com/revature/Project2/service/UserService.java
@@ -0,0 +1,34 @@
+package com.revature.Project2.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.Project2.beans.User;
+import com.revature.Project2.repository.UserRepo;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserRepo uRepo;
+	
+	public Optional<User> getUser(int uId){
+		System.out.println("UserService -getUser");
+		return uRepo.findById(uId);
+	}
+	
+	public List<User> retrieveAllUsers(){
+		System.out.println("UserService - retrieveAllUsers");
+		return (List<User>) uRepo.findAll();
+	}
+	
+	public User createUser(User us){
+		System.out.println("UserService -createUser");
+		uRepo.save(us);
+		return us;
+	}
+
+}
