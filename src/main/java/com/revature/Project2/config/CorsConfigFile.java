7c6a3e1eea144b027bb277cd0b273fa0cf337b4d
Chinedu Ozodi <chinedu.ozodi@gmail.com>
2018-09-16 03:51:58 -0400

    Fixed conflict with CORS and swagger, added redirection on '/' endpoint

--- src/main/java/com/revature/Project2/config/CorsConfigFile.java
+++ src/main/java/com/revature/Project2/config/CorsConfigFile.java
@@ -3,15 +3,31 @@ package com.revature.Project2.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class CorsConfigFile implements WebMvcConfigurer {
 
+	/**
+	 * Allows users to get past CORS error
+	 */
 	@Override
-	   public void addCorsMappings(CorsRegistry registry) {
-	       registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
 
-	   }
+	}
+
+	/**
+	 * Fixes errors with having the @EnableWebMvc and trying to view the swagger ui
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+	}
 }
